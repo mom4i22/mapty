10ea0f469e87e75f1000e3ed20b0ad13e4cd88d6
mtrenchev <M.Trenchev@ibs.bg>
2022-10-06 12:40:43 +0300

    deleted some stuff

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
